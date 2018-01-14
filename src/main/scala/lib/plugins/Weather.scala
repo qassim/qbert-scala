9554ca8c0187b43a48a14c67fd29b3e44e893469
Qassim Farid <me@qassim.uk>
2018-01-14 00:06:22 +0000

    bodge weather

--- src/main/scala/lib/plugins/Weather.scala
+++ src/main/scala/lib/plugins/Weather.scala
@@ -14,21 +14,47 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import org.json4s._
 import org.json4s.native.JsonMethods._
 
+import scala.util.{Failure, Success}
+
 
 class Weather extends Plugin {
   private val conf = ConfigFactory.load()
 
-  case class Address_components(long_name: String, short_name: String, types: List[String])
-  case class Northeast(lat: Double, lng: Double)
-  case class Bounds(northeast: Northeast, southwest: Northeast)
-  case class Geometry(bounds: Bounds, location: Northeast, location_type: String, viewport: Bounds)
-  case class Results(address_components: List[Address_components], formatted_address: String, geometry: Geometry, place_id: String, types: List[String])
-  case class RootLocationJSON(results: List[Results], status: String)
+  case class Address_components(
+                                 long_name: String,
+                                 short_name: String,
+                                 types: List[String]
+                               )
+  case class Location(
+                       lat: Double,
+                       lng: Double
+                     )
+  case class Viewport(
+                       northeast: Location,
+                       southwest: Location
+                     )
+  case class Geometry(
+                       location: Location,
+                       location_type: String,
+                       viewport: Viewport
+                     )
+  case class Results(
+                      address_components: List[Address_components],
+                      formatted_address: String,
+                      geometry: Geometry,
+                      place_id: String,
+                      types: List[String]
+                    )
+  case class RootLocationJSON(
+                             results: List[Results],
+                             status: String
+                           )
 
-  case class Currently(time: Double, summary: String, icon: String, nearestStormDistance: Double, nearestStormBearing: Double, precipIntensity: Double, precipProbability: Double, temperature: Double,
+
+  case class Currently(time: Double, summary: Option[String], icon: String, nearestStormDistance: Option[Double], nearestStormBearing: Option[Double], precipIntensity: Double, precipProbability: Double, temperature: Double,
                         apparentTemperature: Double, dewPoint: Double, humidity: Double, pressure: Double, windSpeed: Double, windGust: Double, windBearing: Double, cloudCover: Double, uvIndex: Double, visibility: Double, ozone: Double)
   case class Data(time: Double, precipIntensity: Double, precipProbability: Double)
-  case class Minutely(summary: String, icon: String, data: List[Data])
+  case class Minutely(summary: Option[String], data: List[Data])
   case class Flags(sources: List[String], `isd-stations`: List[String], units: String)
   case class RootWeatherJSON(latitude: Double, longitude: Double, timezone: String, currently: Currently, minutely: Minutely, hourly: Minutely, daily: Minutely, flags: Flags, offset: Double)
 

0a70406da0a884f9f6d012206ce253fc981a19c7
Qassim Farid <me@qassim.uk>
2018-01-26 21:48:46 +0000

    sysinfo plugin

---
+++ src/main/scala/lib/plugins/SysInfo.scala
@@ -0,0 +1,23 @@
+package lib.plugins
+
+import lib.Plugin
+import slack.models.Message
+import slack.rtm.SlackRtmClient
+
+class SysInfo extends Plugin {
+  def name(): String = "sysinfo"
+  def pluginType(): String = "command"
+
+
+  def action(message: Message, args: String, client: SlackRtmClient) = {
+    val mb = 1024*1024
+    val runtime = Runtime.getRuntime
+    val usedMemory = (runtime.totalMemory - runtime.freeMemory) / mb
+    val freeMemory = runtime.freeMemory / mb
+    val totalMemory = runtime.totalMemory / mb
+    val maxMemory = runtime.maxMemory / mb
+
+    client.sendMessage(message.channel, s"*Memory* | *Used*: ${usedMemory}MB | *Free*: ${freeMemory}MB | *Total*: ${totalMemory}MB | *Max available*: ${maxMemory}MB")
+
+  }
+}

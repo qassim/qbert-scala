2333d8350d9068e41a5c42f29ed80ba5efab584b
Qassim Farid <me@qassim.uk>
2018-03-01 21:22:30 +0000

    Add apparentTemperature and icons to weather plugin

--- src/main/scala/lib/plugins/Weather.scala
+++ src/main/scala/lib/plugins/Weather.scala
@@ -31,8 +31,20 @@ class Weather extends Plugin {
     results.map { result =>
       client.sendMessage(message.channel,
         s"*${result.address}* | " +
-        s"*Temperature*: ${result.weather.currently.temperature}°C. | " +
-        s"*Currently*: ${result.weather.currently.summary getOrElse "No summary"} | " +
+        s"*Currently*: ${result.weather.currently.summary getOrElse "No summary"} ${result.weather.currently.icon match {
+          case "rain" => ":rain_cloud:"
+          case "snow" => ":snow_cloud:"
+          case "clear-day" => ":sunny:"
+          case "clear-night" => ":sunny: :moon:"
+          case "sleet" => ":snow_cloud:"
+          case "wind" => ":wind_blowing_face:"
+          case "fog" => ":fog:"
+          case "cloudy" => ":cloud:"
+          case "partly-cloudy-day" => ":partly_sunny:"
+          case "partly-cloudy-night" => ":partly_sunny: :moon:"
+          case _ => ""
+        }} | " +
+        s"*Temperature*: ${result.weather.currently.temperature}°C. *Feels like*: ${result.weather.currently.apparentTemperature}°C | " +
         s"*Humidity*: ${result.weather.currently.humidity * 100}% | " +
         s"*Day*: ${result.weather.hourly.summary getOrElse "No day summary"}")
     }
@@ -50,4 +62,6 @@ class Weather extends Plugin {
       Http(s"https://maps.googleapis.com/maps/api/geocode/json?address=${locationString}&key=${conf.getString("plugin.weather.gmapi")}").asString
     parse(locationRequest.body).extract[WeatherAPIModel.RootLocationJSON].results(0)
   }
+
+
 }

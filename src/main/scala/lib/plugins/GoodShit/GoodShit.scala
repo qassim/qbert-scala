5bea9238f8c5635798166e8f9329039709e1abec
Qassim Farid <qassim.farid@bbc.co.uk>
2018-03-13 12:46:00 +0000

    Some good shit.

---
+++ src/main/scala/lib/plugins/GoodShit/GoodShit.scala
@@ -0,0 +1,15 @@
+package lib.plugins.TestPlugin
+
+import lib.Plugin
+import slack.models.Message
+import slack.rtm.SlackRtmClient
+
+class TestPlugin extends Plugin {
+
+  def name(): String = "goodshit"
+  def action(message: Message, args: String, client: SlackRtmClient) = {
+    val goodshit = "👌👀👌👀👌👀👌👀👌👀 good shit go౦ԁ sHit👌 thats ✔ some good👌👌shit right👌👌there👌👌👌 right✔there ✔✔if i do ƽaү so my self 💯 i say so 💯 thats what im talking about right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ💯 👌👌 👌НO0ОଠOOOOOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ👌 👌👌 👌 💯 👌 👀 👀 👀 👌👌Good shit"
+    client.sendMessage(message.channel, goodshit)
+  }
+  def pluginType(): String = "command"
+}

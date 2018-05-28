38e0d9368e0fe48611533ce323587ac380ff0038
Qassim Farid <me@qassim.uk>
2018-05-28 21:04:05 +0100

    Add uptime to sysinfo

--- src/main/scala/lib/plugins/SysInfo/SysInfo.scala
+++ src/main/scala/lib/plugins/SysInfo/SysInfo.scala
@@ -3,6 +3,7 @@ package lib.plugins.SysInfo
 import lib.Plugin
 import slack.models.Message
 import slack.rtm.SlackRtmClient
+import java.lang.management.ManagementFactory
 
 class SysInfo extends Plugin {
   def name(): String = "sysinfo"
@@ -12,12 +13,16 @@ class SysInfo extends Plugin {
   def action(message: Message, args: String, client: SlackRtmClient) = {
     val mb = 1024*1024
     val runtime = Runtime.getRuntime
+    val runtimeBean = ManagementFactory.getRuntimeMXBean
     val usedMemory = (runtime.totalMemory - runtime.freeMemory) / mb
     val freeMemory = runtime.freeMemory / mb
     val totalMemory = runtime.totalMemory / mb
     val maxMemory = runtime.maxMemory / mb
+    val uptime = runtimeBean.getUptime
 
-    client.sendMessage(message.channel, s"*Memory* | *Used*: ${usedMemory}MB | *Free*: ${freeMemory}MB | *Total*: ${totalMemory}MB | *Max available*: ${maxMemory}MB")
+
+
+    client.sendMessage(message.channel, s"*Memory* | *Used*: ${usedMemory}MB | *Free*: ${freeMemory}MB | *Total*: ${totalMemory}MB | *Max available*: ${maxMemory}MB | *Uptime*: ${(uptime / 1000) / 60} minutes")
 
   }
 }

5a398663965435d83535fde7625b60e5044fd7c0
Qassim Farid <me@qassim.uk>
2018-06-09 19:14:32 +0100

    More refactoring and descriptive commit logs

--- src/main/scala/lib/plugins/Metrolink/Metrolink.scala
+++ src/main/scala/lib/plugins/Metrolink/Metrolink.scala
@@ -7,7 +7,6 @@ import org.json4s.native.JsonMethods._
 import slack.models.Message
 import slack.rtm.SlackRtmClient
 
-import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scalaj.http._
@@ -16,7 +15,9 @@ class Metrolink extends Plugin {
   val name = "Metrolink"
   val pluginType = "command"
 
+  private implicit val formats = DefaultFormats
   private val conf = ConfigFactory.load()
+  private val shortcuts = Map("deansgate" -> "Deansgate - Castlefield")
 
   def action(message: Message, args: String, client: SlackRtmClient) = apiRequest.map(result =>
     args match {
@@ -25,46 +26,38 @@ class Metrolink extends Plugin {
     })
 
   private def apiRequest(): Future[String] = Future {
-    val response: HttpResponse[String] =
-      Http("https://api.tfgm.com/odata/Metrolinks?")
-        .header("Ocp-Apim-Subscription-Key", conf.getString("plugin.metrolink.apikey"))
-        .asString
-    response.body
+    Http("https://api.tfgm.com/odata/Metrolinks?")
+      .header("Ocp-Apim-Subscription-Key", conf.getString("plugin.metrolink.apikey"))
+      .asString
+      .body
   }
 
   private def constructResponse(args: String, response: String) = {
-    val responseList = parseResponse(response, args)
-    responseList.map { response =>
-      var resultString = ""
-      resultString += s"*Trams due to depart from ${response.StationLocation}*\n"
-      if (response.Dest0 != "") resultString += s"*Platform*\n ```[${response.Carriages0}] ${response.Dest0} departs in ${response.Wait0} minutes\n"
-      if (response.Dest1 != "") resultString += s"[${response.Carriages1}] ${response.Dest1} departs in ${response.Wait1} minutes\n"
-      if (response.Dest2 != "") resultString += s"[${response.Carriages2}] ${response.Dest2} departs in ${response.Wait2} minutes\n"
-      resultString += "```"
-      resultString += s"*Message Board*: ${response.MessageBoard}"
-      resultString
-    }.mkString("\n")
+    var messageBoardString = ""
+    parseResponse(response, args)
+      .map { response =>
+        var resultString = ""
+        resultString += s"*Trams due to depart from ${response.StationLocation}*\n"
+        if (response.Dest0 != "") resultString += s"*Platform*\n ```[${response.Carriages0}] ${response.Dest0} departs in ${response.Wait0} minutes\n"
+        if (response.Dest1 != "") resultString += s"[${response.Carriages1}] ${response.Dest1} departs in ${response.Wait1} minutes\n"
+        if (response.Dest2 != "") resultString += s"[${response.Carriages2}] ${response.Dest2} departs in ${response.Wait2} minutes\n"
+        resultString += "```"
+        messageBoardString = s" *Message Board*: ${response.MessageBoard}\n"
+        resultString
+      }.mkString("\n") + messageBoardString
   }
 
   private def parseResponse(body: String, location: String) = {
     val trains = parseJson(body)
     val locationString = parseShort(location).getOrElse(location)
-    
+
     trains.value
-          .filter(_.StationLocation.toLowerCase == locationString.toLowerCase)
-          .filter(_.Dest0.nonEmpty).distinct
+      .filter(_.StationLocation.toLowerCase == locationString.toLowerCase)
+      .filter(_.Dest0.nonEmpty).distinct
   }
 
-
-  private def parseShort(location: String) = {
-    val shorts = Map("deansgate" -> "Deansgate - Castlefield")
-    shorts.get(location.toLowerCase)
-  }
-
-  private def parseJson(body: String) = {
-    implicit val formats = DefaultFormats
-    parse(body).extract[MetrolinkAPIModel.Trains]
-  }
+  private def parseShort(location: String) = shortcuts.get(location.toLowerCase)
+  private def parseJson(body: String) = parse(body).extract[MetrolinkAPIModel.Trains]
 
   private def getAllLocations(body: String) = {
     val trains = parseJson(body)

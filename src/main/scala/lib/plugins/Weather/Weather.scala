7e4012018b50d08fb553a52554b75f7b9d1fc9b3
Qassim Farid <qassim.farid@bbc.co.uk>
2018-06-15 15:54:40 +0100

    Weather module refactoring

--- src/main/scala/lib/plugins/Weather/Weather.scala
+++ src/main/scala/lib/plugins/Weather/Weather.scala
@@ -21,22 +21,23 @@ class Weather extends Plugin {
   val name = "Weather"
   val pluginType = "command"
 
-  def action(message: Message, args: String, client: SlackRtmClient): Unit = for {
+  def action(message: Message, args: String, client: SlackRtmClient): Unit = {
+    for {
       locationData <- getLocation(args)
       result <- getWeather(locationData)
-    } yield client.sendMessage(message.channel,
+    } yield {
+      client.sendMessage(message.channel,
         s"*${result.address}* | " +
           s"*Currently*: ${result.weather.currently.summary getOrElse "No summary"} ${getIcon(result.weather.currently.icon)} | " +
           s"*Temperature*: ${result.weather.currently.temperature}°C. *Feels like*: ${result.weather.currently.apparentTemperature}°C | " +
           s"*Humidity*: ${result.weather.currently.humidity * 100}% | " +
           s"*Day*: ${result.weather.hourly.summary getOrElse "No day summary"}")
+    }
+  }
 
   private def getWeather(locationData: WeatherAPIModel.Results) = Future {
-    val weatherRequest: HttpResponse[String] = Http(
-      s"https://api.darksky.net/forecast/" +
-        s"${conf.getString("plugin.weather.fcio")}/" +
-        s"${locationData.geometry.location.lat},${locationData.geometry.location.lng}?units=uk2")
-      .asString
+    val requestURL = s"https://api.darksky.net/forecast/${conf.getString("plugin.weather.fcio")}/${locationData.geometry.location.lat},${locationData.geometry.location.lng}?units=uk2"
+    val weatherRequest: HttpResponse[String] = Http(requestURL).asString
 
     val weatherData = parse(weatherRequest.body).extract[WeatherAPIModel.RootWeatherJSON]
     WeatherAPIModel.weatherResult(locationData.formatted_address, weatherData)
@@ -44,17 +45,15 @@ class Weather extends Plugin {
 
   private def getLocation(location: String) = Future {
     val locationString = URLEncoder.encode(location, "UTF-8")
-    val locationRequest: HttpResponse[String] =
-      Http(s"https://maps.googleapis.com/maps/api/geocode/json?address=$locationString&key=${conf.getString("plugin.weather.gmapi")}")
-        .asString
+    val requestURL = s"https://maps.googleapis.com/maps/api/geocode/json?address=$locationString&key=${conf.getString("plugin.weather.gmapi")}"
+    val locationRequest: HttpResponse[String] = Http(requestURL).asString
     parse(locationRequest.body)
       .extract[WeatherAPIModel.RootLocationJSON]
       .results
       .head
   }
 
-  private def getIcon(icon: String) = {
-    icon match {
+  private def getIcon(icon: String) = icon match {
       case "rain" => ":rain_cloud:"
       case "snow" => ":snow_cloud:"
       case "clear-day" => ":sunny:"
@@ -67,5 +66,4 @@ class Weather extends Plugin {
       case "partly-cloudy-night" => ":partly_sunny: :moon:"
       case _ => ""
     }
-  }
 }
